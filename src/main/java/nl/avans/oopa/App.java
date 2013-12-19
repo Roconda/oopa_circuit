9fd5e6076733c8d7460fcb31341581031d69c7c9
Tim Slot <tim.slot@gmail.com>
2013-12-19 10:44:37 +0100

    added finite state machine

--- src/main/java/nl/avans/oopa/App.java
+++ src/main/java/nl/avans/oopa/App.java
@@ -5,24 +5,20 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import nl.avans.oopa.model.Factory;
 import nl.avans.oopa.model.Node;
 
 public class App {
 
-
 	public static void main(String[] args) {
-		Map<String, Node> map = new HashMap<String, Node>();
-		boolean isNew = true;
+		Node uselessNode = Factory.create("AND"); // hack
 
-		if(args.length == 1){
+		if (args.length == 1) {
 			// read text file
 			String name = args[0];
 			// IO vars
-			File f = new File (name);
+			File f = new File(name);
 			BufferedReader br = null;
 
 			try {
@@ -31,53 +27,25 @@ public class App {
 				br = new BufferedReader(fr);
 				String output;
 
-				while ( (output = br.readLine()) != null) {					
-					String[] commands = output.split("\\s+"); // remove spaces
-					String identifier = ""; // identifier / variable
-					Node linkNode;
-					
-					for(String command : commands) {
-						if(command.contains("#")) break; // ignore comments
-						
-						// New line
-						if(isNew) {
-							if(map.get(command) == null) {
-								// create node identifier
-								identifier = command.replace(":", "");
-								isNew = false;
-							}else{
-								System.out.println("Could not find identifier: " + command.replace(":", ""));
-								break;
-							}
-						}else{
-							// link nodes
-							// TODO: split on ','
-							if(command.contains(";")) {
-								// initiate node
-								map.put(identifier, Factory.create(command.replace(";", "")));
-								System.out.println("+"+command.replace(";", "")+"+");
-								isNew = false;
-							}
-						}
-						
-						
-						System.out.println(command);
-					}
+				while ((output = br.readLine()) != null) {					
+					Parser.parse(output);
 				}
 
-			}catch (FileNotFoundException e) {
+			} catch (FileNotFoundException e) {
 				System.out.println("File not found");
 				System.exit(2);
-			}catch (IOException e) {
+			} catch (IOException e) {
 				System.out.println("Unable to read file");
 				System.exit(3);
 			}
 
 			System.exit(0);
-		}else System.out.println("Available arguments: <text file>");
+		} else
+			System.out.println("Available arguments: <text file>");
 
 		// Gone wrong
 		System.exit(1);
 
 	}
+
 }
